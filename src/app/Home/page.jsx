d487c3f7ff386c8e7ea798fa469fdb7c0ab4d5b6
Isaac <iabbeytetteh@gmail.com>
2024-06-26 20:00:23 +0100

    Navbar

---
+++ src/app/Home/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+const Home = () => {
+  return (
+    <div className="h-full w-full bg-[#1D1D1D]">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { MdLeaderboard } from "react-icons/md";
+import Link from "next/link"; // Import Link from next/link
+import { doSignOut } from "../firebase/auth";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const router = useRouter();
+  const handleSignOut = async () => {
+    try {
+      await doSignOut();
+      router.push("/Login");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+  return (
+    <nav className="flex items-center justify-between py-5 px-5 md:px-10 shadow-lg bg-[#272626]">
+      <h1 className="text-white font-extrabold text-md md:text-xl">
+        Football Guess Who
+      </h1>
+      <div className="flex gap-4 text-white items-center">
+        <MdLeaderboard className="cursor-pointer h-8 w-8" />
+        <button
+          onClick={handleSignOut}
+          className="py-2 px-3 text-white rounded-lg border-2 border-white"
+        >
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
